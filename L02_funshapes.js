eba39df4fc90565cda1a79d77933fc9bebc1d894
Computhink-CS135 <cs135@computhink.com.sg>
2026-07-11 07:05:09 +0000

    Jul 11, 2026, 7:05 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -17,7 +17,7 @@ function draw() {
   circle(400, 75, 100);
   circle(250, 200, 300);
   circle(175, 175, 50);
-  circle(225, 225, 50);
+  circle(225, 175, 50);
 
   // Challenge 3: A house using rect() function
 
