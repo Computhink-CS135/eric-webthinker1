d30afa5ad3acdfb7fa09000a66bc350ebdc84090
Computhink-CS135 <cs135@computhink.com.sg>
2026-07-11 07:25:23 +0000

    Jul 11, 2026, 7:25 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -28,7 +28,7 @@ function draw() {
   // circle(250, 260, 25);
 
   // Challenge 4: A rocketship using triangle() function
-  triangle(300, 10, 400, 100, 150, 200)
+  triangle(300, 10, 400, 110, 150, 210)
   ellipse(300,200,150,300)
 
   // Challenge 5: A square face using quad() function
