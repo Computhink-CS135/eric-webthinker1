30b4278f66601851411b151564aadfd0d5dcc27f
Computhink-CS135 <cs135@computhink.com.sg>
2026-07-11 06:43:20 +0000

    Jul 11, 2026, 6:43 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -9,7 +9,7 @@ function setup() {
 function draw() {
   // Challenge 1: An eye using ellipse() function
   ellipse(300,200,200,100);
-  fill(255, 255, 0);
+  fill("#2e1900");
   ellipse(300,200,100,100)
   // Challenge 2: A face using circle() function
 
