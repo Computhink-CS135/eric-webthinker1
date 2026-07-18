e5c3feafacde632727a7b0b55d40b6001a1f45a3
Computhink-CS135 <cs135@computhink.com.sg>
2026-07-18 06:18:34 +0000

    Jul 18, 2026, 6:18 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -37,7 +37,7 @@ function draw() {
   quad(500,50, 100,50, 100,350, 500,350);
   circle(200, 175, 50);
   circle(400, 175, 50);
-  quad(450,250, 150,250, 150,300, 500,300);
+  quad(450,250, 150,250, 200,300, 400,300);
 
   // Challenge 6: Add some colours to the drawings in the above challenges
 
