380f11952cee57a2451ff56102c081f7c9c8cfe3
Computhink-CS135 <cs135@computhink.com.sg>
2026-07-18 06:39:26 +0000

    Jul 18, 2026, 6:39 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -43,7 +43,7 @@ function draw() {
   circle(200, 175, 50);
   circle(400, 175, 50);
   strokeWeight(3);
-  stroke("#ff9c56");
+  stroke("#ff8b56");
   fill("#ff5656");
   quad(450,250, 150,250, 200,300, 400,300);
 
