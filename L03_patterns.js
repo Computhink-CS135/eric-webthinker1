402f7952de43dbb3b38067eceadaaaf04ed02391
Computhink-CS135 <cs135@computhink.com.sg>
2026-07-18 06:46:26 +0000

    Jul 18, 2026, 6:46 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -7,12 +7,12 @@ function setup() {
   
 function draw() {
   // Recap 1: Repeating Circles
-
+  
   // Task 1: Colour Gradient
 
   // Task 2: Colour Loop
 
-  // Ta sk 3: Row of Circles
+  // Task 3: Row of Circles
 
   // Task 4: Grid of Circles
 }
