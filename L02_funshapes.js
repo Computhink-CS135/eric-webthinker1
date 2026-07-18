b2c973abc11e9e5a3ebc18333a4f968d1cff7d16
Computhink-CS135 <cs135@computhink.com.sg>
2026-07-18 06:31:01 +0000

    Jul 18, 2026, 6:31 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -34,6 +34,7 @@ function draw() {
   // rect(200, 300, 200, 50)
   
   // Challenge 5: A square face using quad() function
+  stroke("#ebd1ab")
   fill("#f7ddb5")
   quad(500,50, 100,50, 100,350, 500,350);
   fill("#000000")
