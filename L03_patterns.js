aa79cb85d0020559868a89037492cfda84dc1097
Computhink-CS135 <cs135@computhink.com.sg>
2026-07-25 07:35:56 +0000

    Jul 25, 2026, 7:35 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -22,9 +22,9 @@ function draw() {
   // }
 
   // Task 2: Colour Loop
-  // circle(100, 100, 100);
-  // fill(colorValue);
-  // colorValue++;
+  circle(100, 100, 100);
+  fill(colorValue);
+  colorValue++;
 
   // Task 3: Row of Circles
   // for (let i = 0; i < 10; i++) {
