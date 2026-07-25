2793e8ab22665703cd1eb64d53f7ac7cbf5a8abb
Computhink-CS135 <cs135@computhink.com.sg>
2026-07-25 06:50:44 +0000

    Jul 25, 2026, 6:50 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -35,5 +35,5 @@ function draw() {
   for (let row = 0; row < 5; row++)
     for (let col = 0; col < 5; col++)
       // fill(row % 2 * 255)
-      rect(col * 50, row * 50, 80, 80)
+      rect(col * 80, row * 80, 80, 80)
 }
