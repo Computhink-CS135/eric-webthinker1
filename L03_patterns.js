2867dee2602d900fce5290796cc7548557e203f5
Computhink-CS135 <cs135@computhink.com.sg>
2026-07-25 06:48:44 +0000

    Jul 25, 2026, 6:48 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -33,7 +33,7 @@ function draw() {
 
   // Task 4: Grid of Squares
   for (let row = 0; row < 5; row++)
-    for (let col = 0; col < 5; )
-    fill(row % 2 * 255)
-    rect(0, 0, 100, 100)
+    for (let col = 0; col < 5; col++)
+      fill(row % 2 * 255)
+      rect(col, ro, 100, 100)
 }
