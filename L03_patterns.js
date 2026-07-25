376c77fc6fe1dbbdae5d1dcd53c350ac7f440d14
Computhink-CS135 <cs135@computhink.com.sg>
2026-07-25 07:34:59 +0000

    Jul 25, 2026, 7:34 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -56,7 +56,7 @@ function draw() {
 
     for (let i = 0; i < numCircles; i++) {
       let x = startX + i * circleDiameter;
-      ellipse(x, erowY, circleDiameter, circleDiameter);
+      ellipse(x, rowY, circleDiameter, circleDiameter);
     }
   }
 }
