5133e476a8cc440962f253da5182ca2965cf7adb
Computhink-CS135 <cs135@computhink.com.sg>
2026-08-01 07:28:06 +0000

    Aug 1, 2026, 7:28 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -41,7 +41,7 @@ function setup() {
 // }
 
 function mouseDragged() {
-    let randomsize = random;
+    let randomsize = random(50);
     let randomcolour = color(random(255), random(255), random(255));
     fill(randomcolour)
     ellipse(mouseX, mouseY, 20, 20)
