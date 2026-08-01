16257f21e29de78ebc8942be80881e67c9070bc4
Computhink-CS135 <cs135@computhink.com.sg>
2026-08-01 07:08:03 +0000

    Aug 1, 2026, 7:08 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -22,7 +22,7 @@ function setup() {
 function mouseMoved() {
     let circlesize = 0
     let perivousmouseX = mouseX
-    if ()
+    if (perivousmouseX < mouseX)
     circle(mouseX, mouseY, circlesize);
     circlesize++
 
