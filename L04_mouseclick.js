72869c163870474144936720d63666b8f3f5c14c
Computhink-CS135 <cs135@computhink.com.sg>
2026-08-01 06:17:20 +0000

    Aug 1, 2026, 6:17 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -3,12 +3,12 @@ let shapecolour = #ff0000
 function setup() {
     createCanvas(600, 400);
     background(220);
-  noLoop(); // Stops continuous drawing
+//   noLoop(); // Stops continuous drawing
 }
 function draw() {
     fill(shapecolour);
     ellipse(200, 200, 80, 80);
 }
 function mousePressed() {
-    sha
+    shapecolour
 }
