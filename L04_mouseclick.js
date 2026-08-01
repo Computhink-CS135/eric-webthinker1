87a8205c6dafc15213d98f676e15f03e4900e97d
Computhink-CS135 <cs135@computhink.com.sg>
2026-08-01 06:24:30 +0000

    Aug 1, 2026, 6:24 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -10,5 +10,5 @@ function draw() {
     ellipse(200, 200, 80, 80);
 }
 function mousePressed() {
-    let randomcolour = color(random(255),random())
+    let randomcolour = color(random(255), random(255), random(255)
 }
