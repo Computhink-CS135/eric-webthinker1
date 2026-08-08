24e39a0cbe87cf6ddf298fb6bf9d9ebebfc33170
Computhink-CS135 <cs135@computhink.com.sg>
2026-08-08 07:28:37 +0000

    Aug 8, 2026, 7:28 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -24,7 +24,7 @@ function keyPressed() {
     //     ellipse(positionX, positionY, size, size);
     // }
 
-    console.log()
+    console.log("Key:")
     if (KeyCode === 0) {
         circle(300, 200, 200)
     }
