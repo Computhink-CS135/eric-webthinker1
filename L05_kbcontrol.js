7025b30b30f1ab12ac6963ff472909c6e6ded758
Computhink-CS135 <cs135@computhink.com.sg>
2026-08-08 07:06:09 +0000

    Aug 8, 2026, 7:06 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -28,7 +28,7 @@ function keyPressed() {
         circle(100, 300, 200)
     }
     else {
-        rect(450,100, 450, 300, 150, 300) 
+        rect(450,100, 450, 300, 150,300, ) 
     }
 }
 function keyReleased() {
