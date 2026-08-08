ea9d8540fb8f2ebac0ea38b9f82d8e2d46b6acdb
Computhink-CS135 <cs135@computhink.com.sg>
2026-08-08 06:37:23 +0000

    Aug 8, 2026, 6:37 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -18,7 +18,7 @@ function keyPressed() {
     let positionY = random(0, 400)
     let size = random(10, 25)
     let numCircle = random(5, 20)
-    let randomColour = r
+    let randomColour = random
     for (let i = 0; i < numCircle; i++) {
         ellipse(positionX, positionY, size, size);
     }
