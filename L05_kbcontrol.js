f1647122192008de0a2ede3961758a2888385f8d
Computhink-CS135 <cs135@computhink.com.sg>
2026-08-08 06:17:41 +0000

    Aug 8, 2026, 6:17 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -1,4 +1,5 @@
 
+
 function setup() {
     createCanvas(600, 400);
     background(225);
@@ -6,6 +7,6 @@ function setup() {
 }
 
 function draw() {
-    background(220)
-    rect(width / 2 - rectsize / 2, height / 2 - rectsize / 2, rectsize, rectsize)
+    background(220);
+    rect(width / 2 - rectsize / 2, height / 2 - rectsize / 2, rectsize, rectsize);
 }
