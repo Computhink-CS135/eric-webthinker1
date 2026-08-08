509042cd2128f645b51f71337129ede47b45fbdd
Computhink-CS135 <cs135@computhink.com.sg>
2026-08-08 06:32:36 +0000

    Aug 8, 2026, 6:32 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -14,8 +14,8 @@ function draw() {
 function keyPressed() {
     // rectsize = 100
 
-    let positionX = random(600)
-    let positionY = random(400)
+    let positionX = random(0, 600)
+    let positionY = random(0, 400)
     let size = (10, 25)
     let numCircle = (5, 20)
     for (let i = 0; i < numCircle; i++) {
