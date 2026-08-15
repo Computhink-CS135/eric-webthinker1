b5d10c2f9f1318aa46f222ea9d6cc7e762858866
Computhink-CS135 <cs135@computhink.com.sg>
2026-08-15 07:16:17 +0000

    Aug 15, 2026, 7:16 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -16,7 +16,7 @@ function draw() {
     } else if (keyIsDown(LEFT_ARROW)) {
         x -= 5;
     }
-    triangle(200, 400, 400, 200, 600, 400);
+    triangle(100, 400, 300, 200, 500, 400);
 
     if (x < 50) {
         x = 50;
