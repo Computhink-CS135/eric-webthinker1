2e89fb99ab6584aba221c526ecb13038657857db
Computhink-CS135 <cs135@computhink.com.sg>
2026-08-15 06:56:50 +0000

    Aug 15, 2026, 6:56 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -18,10 +18,12 @@ function draw() {
     // }
     // circle(width / 2 + x, height / 2, 100)
 
-    // if(keyCode == RIGHT_ARROW) {
-    //     x += 1
-    // }
-    // circle(width / 2 + x, height / 2, 100)
+    if(keyCode == RIGHT_ARROW) {
+        x += 1
+    } else if (keyCode == RIGHT_ARROW) {
+        x += 1
+    }
+    circle(width / 2 + x, height / 2, 100)
 }
 
 function keyPressed() {
