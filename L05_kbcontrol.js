7367c556b869ea02b264071e1396b3d082ff6297
Computhink-CS135 <cs135@computhink.com.sg>
2026-08-15 06:47:36 +0000

    Aug 15, 2026, 6:47 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -58,18 +58,18 @@ function keyReleased() {
 }
 
 function mouseMoved() {
-    if (mouseX < 100) {
-        fill(255, 0, 0)
-    } else if (mouseX >= 100 && mouseX < 200) {
-        fill(255, 255, 0)
-    } else if (mouseX >= 200 && mouseX < 300) {
-        fill(0, 255, 0)
-    } else if (mouseX >= 300 && mouseX < 400) {
-        fill(0, 0, 255)
-    } else if (mouseX >= 400 && mouseX < 500) {
-        fill(255, 0, 255)
-    } else if (mouseX >= 500 && mouseX < 600) {
-        fill(255, 0, 220)
-    }
-    circle(300, 200, 200)
+    // if (mouseX < 100) {
+    //     fill(255, 0, 0)
+    // } else if (mouseX >= 100 && mouseX < 200) {
+    //     fill(255, 255, 0)
+    // } else if (mouseX >= 200 && mouseX < 300) {
+    //     fill(0, 255, 0)
+    // } else if (mouseX >= 300 && mouseX < 400) {
+    //     fill(0, 0, 255)
+    // } else if (mouseX >= 400 && mouseX < 500) {
+    //     fill(255, 0, 255)
+    // } else if (mouseX >= 500 && mouseX < 600) {
+    //     fill(255, 0, 220)
+    // }
+    // circle(300, 200, 200)
 }
