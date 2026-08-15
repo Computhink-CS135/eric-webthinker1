edc4b3e179b12011a204556a6a0156e9e5372488
Computhink-CS135 <cs135@computhink.com.sg>
2026-08-15 06:35:06 +0000

    Aug 15, 2026, 6:35 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -58,13 +58,13 @@ function keyReleased() {
 }
 
 function mouseMoved() {
-    if (mouseX < 100) {
+    if (mouseX <= 100) {
         fill(255, 0, 0)
-    } else if (100 < mouseX < 200) {
+    } else if (100 < mouseX <= 200) {
         fill(0, 255, 0)
-    } else if (200 < mouseX < 300) {
+    } else if (200 < mouseX <= 300) {
         fill(255, 255, 0)
-    } else if (200 < mouseX < 300) {
+    } else if (200 < mouseX <= 300) {
         fill(0, 0, 255)
     }
     circle(300, 200, 200)
