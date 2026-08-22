10c5cbbc4a0e87d0af2cb726d0fe75e4c734ff39
Computhink-CS135 <cs135@computhink.com.sg>
2026-08-22 06:46:23 +0000

    Aug 22, 2026, 6:46 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -9,20 +9,21 @@ function setup() {
 }
 function draw() {
     
-    if (keyIsDown(RIGHT_ARROW)) {
-        x += 5;
-    } else if (keyIsDown(LEFT_ARROW)) {
-        x -= 5;
-    } else if (keyIsDown(DOWN_ARROW)) {
-        y += 5;
-    } else if (keyIsDown(UP_ARROW)) {
-        y -= 5;
-    }
-    x = constrain(x, 50, 350)
-    y = constrain(y, 50, 350)
-    x = constrain(x, 0, width - 100)
-    y = constrain(y, 0, height - 100)
-    createCanvas(400, 400);
-    background(225);
-    rect(x, y, 50, 50);
+    // if (keyIsDown(RIGHT_ARROW)) {
+    //     x += 5;
+    // } else if (keyIsDown(LEFT_ARROW)) {
+    //     x -= 5;
+    // } else if (keyIsDown(DOWN_ARROW)) {
+    //     y += 5;
+    // } else if (keyIsDown(UP_ARROW)) {
+    //     y -= 5;
+    // }
+    // x = constrain(x, 50, 350)
+    // y = constrain(y, 50, 350)
+    // x = constrain(x, 0, width - 100)
+    // y = constrain(y, 0, height - 100)
+    // createCanvas(400, 400);
+    // background(225);
+    // rect(x, y, 50, 50);
+
 }
