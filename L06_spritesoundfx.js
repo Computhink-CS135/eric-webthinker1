495626d0f772339ac292c14455fe73df49e860fb
Computhink-CS135 <cs135@computhink.com.sg>
2026-08-22 07:09:59 +0000

    Aug 22, 2026, 7:09 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -31,7 +31,7 @@ function draw() {
     createCanvas(400, 400);
     background("lightblue");
     image(staticImage, x, y, 110, 133);
-    Implementconstrain(Width, height)
+    Implementconstrain(x ,y)
 }
 function preload() {
     soundEffect = loadSound('assets/pop.mp3');
