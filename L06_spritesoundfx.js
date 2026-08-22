23d1920ddb8e60d6ee060a91fcee9bf58e554c1c
Computhink-CS135 <cs135@computhink.com.sg>
2026-08-22 06:27:02 +0000

    Aug 22, 2026, 6:27 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -19,7 +19,7 @@ function draw() {
     // } else if (keyIsDown(UP_ARROW)) {
     //     y -= 5;
     // }
-    y = constrain(y, 0, )
+    y = constrain(y, 0, height)
 
     // circle(width / 2 + x, height / 2 + y, 50);
 }
