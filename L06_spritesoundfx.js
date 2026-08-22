5c1cc864569a9fbb60422090c8cf2bc4967960e4
Computhink-CS135 <cs135@computhink.com.sg>
2026-08-22 07:34:37 +0000

    Aug 22, 2026, 7:34 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -30,7 +30,7 @@ function draw() {
 
     if (x === 400 || x === 0 || y === 400 || y === 0) {
         // soundEffect.play();
-        background(255, 0, 0);
+        background('red');
         if (!soundEffect.isplaying()) {
             soundEffect.play();
         }
