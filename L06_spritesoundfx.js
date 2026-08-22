013028be0922af5005e19e862abf9cf0c7fdb9b8
Computhink-CS135 <cs135@computhink.com.sg>
2026-08-22 06:34:58 +0000

    Aug 22, 2026, 6:34 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -20,8 +20,8 @@ function draw() {
     } else if (keyIsDown("SPACE_BAR")) {
         fill(255, 0, 255);
     }
-    x = constrain(x, 0, width)
-    y = constrain(y, 0, height)
+    x = constrain(x, 0, width - 50)
+    y = constrain(y, 0, height - 50)
     x = constrain(x, 0, width - 50)
     y = constrain(y, 0, height - 50)
 
