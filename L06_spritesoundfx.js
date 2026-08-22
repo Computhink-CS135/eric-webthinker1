ec9d6738a1dcc7e0f82915fba6a2a4a5c90b71e3
Computhink-CS135 <cs135@computhink.com.sg>
2026-08-22 06:54:12 +0000

    Aug 22, 2026, 6:54 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -33,5 +33,5 @@ function draw() {
 function preload() {
     soundEffect = loadSound('assets/pop.mp3');
     bgMusic = loadSound('assets/bossaNova.mp3');
-    staticImage = loadImage('assets')
+    staticImage = loadImage('assets/pico-a')
 }
