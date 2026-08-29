2e41d3418ea2b5b486e1836c15847b8aa71dc080
Computhink-CS135 <cs135@computhink.com.sg>
2026-08-29 06:52:06 +0000

    Aug 29, 2026, 6:52 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -1,45 +1,46 @@
 
 //  Recap 1
-let picoImage;
-let popSound;
-let x = 0;
-let y = 0;
-let picoWidth = 100;
+// let picoImage;
+// let popSound;
+// let x = 0;
+// let y = 0;
+// let picoWidth = 100;
 
-function preload() {
-    picoImage = loadImage('assets/pico-a.png');
-    popSound = loadSound('assets/pop.mp3');
-}
-function setup() {
-    createCanvas(600, 400);
-}
-function draw() {
-    background("gray");
+// function preload() {
+//     picoImage = loadImage('assets/pico-a.png');
+//     popSound = loadSound('assets/pop.mp3');
+// }
+// function setup() {
+//     createCanvas(600, 400);
+// }
+// function draw() {
+//     background("gray");
 
-    if (keyIsDown(RIGHT_ARROW)) {
-        x += 5;
-    } else if (keyIsDown(LEFT_ARROW)) {
-        x -= 5;
-    } else if (keyIsDown(DOWN_ARROW)) {
-        y += 5;
-    } else if (keyIsDown(UP_ARROW)) {
-        y -= 5;
-    } 
-    x = constrain(x, 0, width - 100);
-    y = constrain(y, 0, height - 100);
-    createCanvas(600, 400);
-    background("gray");
-    image(picoImage, x, y, picoWidth, 100);
-}
-function keyPressed() {
-    if (keyCode === 32) {
-        popSound.play()
-        picoWidth = 50;
+//     if (keyIsDown(RIGHT_ARROW)) {
+//         x += 5;
+//     } else if (keyIsDown(LEFT_ARROW)) {
+//         x -= 5;
+//     } else if (keyIsDown(DOWN_ARROW)) {
+//         y += 5;
+//     } else if (keyIsDown(UP_ARROW)) {
+//         y -= 5;
+//     } 
+//     x = constrain(x, 0, width - 100);
+//     y = constrain(y, 0, height - 100);
+//     createCanvas(600, 400);
+//     background("gray");
+//     image(picoImage, x, y, picoWidth, 100);
+// }
+// function keyPressed() {
+//     if (keyCode === 32) {
+//         popSound.play()
+//         picoWidth = 50;
 
-    }
-}
-function keyReleased() {
-    if (keyCode === 32) {
-        picoWidth = 100;
-    }
-}
+//     }
+// }
+// function keyReleased() {
+//     if (keyCode === 32) {
+//         picoWidth = 100;
+//     }
+// }
+
