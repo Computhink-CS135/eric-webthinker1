17ee2f9aab22a83ce69aecbecd47f8cc733e56dc
Computhink-CS135 <cs135@computhink.com.sg>
2026-08-29 06:28:44 +0000

    Aug 29, 2026, 6:28 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -24,6 +24,8 @@ function draw() {
     } else if (keyIsDown(UP_ARROW)) {
         y -= 5;
     }
+    x = constrain(x, 0, width - 100);
+    y = constrain(y, 0, height - 100);
     createCanvas(600, 400);
     background("gray");
     image(picoImage, x, y, 100, 100);
