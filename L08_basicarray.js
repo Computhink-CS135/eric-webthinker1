e150d1fc3f24400b64bf5850304819e63028c48f
Computhink-CS135 <cs135@computhink.com.sg>
2026-09-12 07:27:45 +0000

    Sep 12, 2026, 7:27 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -24,7 +24,7 @@ favouriteFoods = ["Pizza", "Burger", "Sushi", "Tacos", "Pasta"];
 function setup() {
     createCanvas(600, 400);
     background(220);
-    // textSize(random(1, 50));
+    textSize(random(1, 50));
     textAlign(CENTER, CENTER);
     yPos = height;
     // text("My name is Eric.", 50, 50);
