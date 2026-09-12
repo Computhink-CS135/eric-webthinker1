5b93aba1980c917ccb2769786660e1684ac55855
Computhink-CS135 <cs135@computhink.com.sg>
2026-09-12 06:45:50 +0000

    Sep 12, 2026, 6:45 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -23,7 +23,7 @@
 function setup() {
     createCanvas(600, 400);
     background(220);
-    textSize(random(20, 100));
+    textSize(random(1, 100));
     text("Ola!", width / 2, height / 2);
 }
 function draw() {
