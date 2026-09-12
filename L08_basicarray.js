e28ed941dfe909f24b077aca9b6683801d528e33
Computhink-CS135 <cs135@computhink.com.sg>
2026-09-12 07:19:54 +0000

    Sep 12, 2026, 7:19 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -35,7 +35,7 @@ function draw() {
     yPos -= 10
     createCanvas(600, 400);
     background(220);
-    textSize(random(1, 50));
+    textSize(random(24, 2));
     text("My favourite foods:", 50, 50);
     for (let i = 0; i < favouriteFoods.length; i++) {
         text((i + 1) + ". " + favouriteFoods[i], 50, yPos + i * 30);
