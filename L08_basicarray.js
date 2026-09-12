9bf866b275acaace13d67098cfd79ebe732b1620
Computhink-CS135 <cs135@computhink.com.sg>
2026-09-12 06:38:56 +0000

    Sep 12, 2026, 6:38 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -5,7 +5,7 @@ let squareY = 0
 let squarespeedX = 5
 let squarespeedY = 5
 function setup() {
-    createCanvas(600, 400);
+    createCanvas(400, 400);
     background(220);
 }
 function draw() {
@@ -13,7 +13,7 @@ function draw() {
     squareY += squarespeedY;
     if (squareX + 100 > width || squareX < 0) {
         squarespeedX *= -1
-    } if (squareY + 100 > height || squareX < 0) {
+    } if (squareY + 100 > height || squareY < 0) {
         squarespeedY *= -1
     }
     fill(255, 0, 255);
