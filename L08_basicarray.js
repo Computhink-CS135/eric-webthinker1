6597e673edf5b8dbbb1b87ac93fb9431be5f1b3e
Computhink-CS135 <cs135@computhink.com.sg>
2026-09-12 07:14:35 +0000

    Sep 12, 2026, 7:14 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -26,7 +26,7 @@ function setup() {
     background(220);
     textSize(random(1, 50));
     textAlign(LEFT, TOP);
-    yPos = height
+    yPos = height;
     // text("My name is Eric.", 50, 50);
     // text("I am 10 this year.", 50, 100);
     // text("My favourite activity is gaming.", 50, 150);
