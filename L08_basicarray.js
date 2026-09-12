db5aa6629c65b94b6253dfe420be26a331d5c456
Computhink-CS135 <cs135@computhink.com.sg>
2026-09-12 06:23:36 +0000

    Sep 12, 2026, 6:23 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -1,11 +1,13 @@
 
 // recap 1
+let squareX = 0
+let squareY = 0 
 function setup() {
     new Canvas(600, 400);
     background(220);
 }
 function draw() {
+    su
     fill(255, 0, 255)
     rect(squareX, squareY, 100, 100);
-
 }
