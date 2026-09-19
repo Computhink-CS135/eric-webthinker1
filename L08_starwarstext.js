44aa933e422f7066381c594458e4bf91d53c75f1
Computhink-CS135 <cs135@computhink.com.sg>
2026-09-19 06:56:44 +0000

    Sep 19, 2026, 6:56 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -4,7 +4,7 @@ let textA = ["ur adopted", "credits:", "Eric #1", "Eric #2", "Eric #3"]
 let bgMusic;
 
 function preLoad() {
-    bgMusic = loadSound("assets/star_wars")
+    bgMusic = loadSound("assets/star_wars_theme_8_")
 }
 function setup() {
     createCanvas(600, 400);
