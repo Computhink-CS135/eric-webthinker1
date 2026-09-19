d14b5ecfd07e4ccaceb81a096916e238e92cc7e1
Computhink-CS135 <cs135@computhink.com.sg>
2026-09-19 06:43:37 +0000

    Sep 19, 2026, 6:43 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -1,6 +1,6 @@
 
 let yPos;
-let text = ["ur adopted", "credits:", "Eric #1", "Eric #2", "Eric #3"]
+let textA = ["ur adopted", "credits:", "Eric #1", "Eric #2", "Eric #3"]
 
 function setup() {
     createCanvas(600, 400);
@@ -13,7 +13,7 @@ function setup() {
 function draw() {
     background(220);
     for (let i = 0; i < text.length; i++) {
-        text((i + 1)+ text[i], 50, yPos + i * 30);
+        text((i + 1)+ textA[i], 50, yPos + i * 30);
     }
     yPos -= 1;
 }
