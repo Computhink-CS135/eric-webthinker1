4b47abaa134fc0401dda94b4a92274683d4143f6
Computhink-CS135 <cs135@computhink.com.sg>
2026-09-19 07:17:15 +0000

    Sep 19, 2026, 7:17 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -1,1 +1,14 @@
 
+function setup() {
+    createCanvas(600, 400);
+    textAlign(LEFT, CENTER);
+    textSize(24);
+    fill("rgb(255, 255, 0)");
+}
+function draw() {
+    background(0);
+    for (let i = 0; i < text.length; i++) {
+        text(textA[i], width / 4, yPos + i * 50);
+    }
+    yPos -= 0.5;
+}
