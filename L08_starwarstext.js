4ba2234c5d479ff88478bb8092a3ab363ed51bdd
Computhink-CS135 <cs135@computhink.com.sg>
2026-09-19 06:19:52 +0000

    Sep 19, 2026, 6:19 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -1,13 +1,13 @@
 
-let yPos
+let yPos = 0;
 
 function setup() {
     createcanvas(600, 400);
     textAlign(LEFT, CENTER);
     textSize(24);
-    fill("rgb(255, 255, 0)")
+    fill("rgb(255, 255, 0)");;
 }
 function draw() {
-    background(220)
-    text("asd", width / 2, yPos)
+    background(220);
+    text("asd", width / 2, yPos);
 }
