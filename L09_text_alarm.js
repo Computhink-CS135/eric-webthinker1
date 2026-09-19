59da093366f73740eaf9addab6368f942c1b906c
Computhink-CS135 <cs135@computhink.com.sg>
2026-09-19 07:28:14 +0000

    Sep 19, 2026, 7:28 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -11,8 +11,10 @@ function draw() {
     min = minute();
     sec = second();
 
+    timeString
+
     background(220)
-    
+
     text(hour, width / 2, height / 2);
     text(" :", width / 2 + 25, height / 2);
     text(min, width / 2 + 50, height / 2);
