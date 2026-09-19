09988a2557057a0fa424e81c4b5044c5da3b2a70
Computhink-CS135 <cs135@computhink.com.sg>
2026-09-19 07:11:18 +0000

    Sep 19, 2026, 7:11 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -21,13 +21,6 @@
 // }
 
 favouriteFoods = ["Pizza", "Burger", "Sushi", "Tacos", "Pasta"];
-
-let bgMusicA;
-
-function preLoad() {
-    bgMusicA = loadSound("assets/star_wars_theme_8_bit.mp3");
-}
-
 function setup() {
     createCanvas(600, 400);
     background(220);
@@ -37,8 +30,6 @@ function setup() {
     // text("My name is Eric.", 50, 50);
     // text("I am 10 this year.", 50, 100);
     // text("My favourite activity is gaming.", 50, 150);
-
-    bgMusicA.loop();
 }
 function draw() {
     yPos -= 0.6
