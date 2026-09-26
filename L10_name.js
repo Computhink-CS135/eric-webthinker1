171451ba55c7fffa8af438f6b671e6dd3a7749f6
Computhink-CS135 <cs135@computhink.com.sg>
2026-09-26 07:34:01 +0000

    Sep 26, 2026, 7:34 AM

--- L10_name.js
+++ L10_name.js
@@ -5,16 +5,16 @@ let inputX = this.CanvasCaptureMediaStreamTrack.offsetLeft + (width / 2) - 80
 let inputY = this.CanvasCaptureMediaStreamTrack.offsetTop + (height / 2) - 10
 
 function setup() {
-    background(220)
+    background(220);
     createCanvas(600, 400);
-    textSize(50)
-    textAlign(CENTER, CENTER)
+    textSize(50);
+    textAlign(CENTER, CENTER);
     inputText = createInput();
     inputText.position(inputX, inputY);
-    inputText.input(updateText)
+    inputText.input(updateText);
 }
 function draw() {
-    Text(displayText, width / 2, height / 2)
+    Text(displayText, width / 2, height / 2);
 }
 function updateText() {
     displayText = this.value();
