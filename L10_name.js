33aef269f3687beaeda991561a099f6f64f50502
Computhink-CS135 <cs135@computhink.com.sg>
2026-09-26 07:17:02 +0000

    Sep 26, 2026, 7:17 AM

--- L10_name.js
+++ L10_name.js
@@ -5,7 +5,7 @@ let displayText = "Your Name Here";
 function setup() {
     createCanvas(600, 400);
     inputText = createInput();
-    inputText.position(width / 2, height / 2)
+    inputText.position(width / 2, height / 2);
 }
 function draw() {
 
