77d96845744c09d5db3f13f9da206eaf0fd70cd0
Computhink-CS135 <cs135@computhink.com.sg>
2026-09-26 07:32:17 +0000

    Sep 26, 2026, 7:32 AM

--- L10_name.js
+++ L10_name.js
@@ -1,7 +1,7 @@
 
 let inputText;
 let displayText = "Your Name Here";
-let inputX = this.CanvasCaptureMediaStreamTrack.offsetLeft + 
+let inputX = this.CanvasCaptureMediaStreamTrack.offsetLeft + (width / 2)
 
 function setup() {
     createCanvas(600, 400);
