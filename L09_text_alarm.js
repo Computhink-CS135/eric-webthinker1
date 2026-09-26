17a013130f4b96857ee5a900b1224de368cc5dc4
Computhink-CS135 <cs135@computhink.com.sg>
2026-09-26 06:14:39 +0000

    Sep 26, 2026, 6:14 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -27,7 +27,7 @@ let x = 0;
 
 function setup() {
     createCanvas(600, 400);
-    intervalid = setInterval(r)
+    intervalid = setInterval(drawCircle, 100)
     background(0);
 }
 function draw() {
