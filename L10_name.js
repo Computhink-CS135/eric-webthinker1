2fa92c55cc391db9b952aa15f14789e3f806f2a6
Computhink-CS135 <cs135@computhink.com.sg>
2026-09-26 07:28:41 +0000

    Sep 26, 2026, 7:28 AM

--- L10_name.js
+++ L10_name.js
@@ -11,7 +11,7 @@ function setup() {
 function draw() {
     background(220)
 
-    Text(displayText, )
+    Text(displayText, width / 2, height / 2)
 }
 function updateText() {
     displayText = this.value();
