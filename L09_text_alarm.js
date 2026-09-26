23adf3b61af35edf8723b5e9e7db1df9ab0a7374
Computhink-CS135 <cs135@computhink.com.sg>
2026-09-26 06:08:56 +0000

    Sep 26, 2026, 6:08 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -1,23 +1,24 @@
 
-function setup() {
-    createCanvas(600, 400);
-    textAlign(LEFT, CENTER);
-    textSize(24);
-    fill("rgb(150, 0, 255)");
-}
-function draw() {
+// function setup() {
+//     createCanvas(600, 400);
+//     textAlign(LEFT, CENTER);
+//     textSize(24);
+//     fill("rgb(150, 0, 255)");
+// }
+// function draw() {
 
-    background(220);
+//     background(220);
 
-    hour = hour();
-    min = minute();
-    sec = second();
+//     hour = hour();
+//     min = minute();
+//     sec = second();
 
-    timeString = nf(hour, 2) + " : " + nf(min, 2) + " : " + nf(sec, 2);
+//     timeString = nf(hour, 2) + " : " + nf(min, 2) + " : " + nf(sec, 2);
 
-    // text(hour, width / 2, height / 2);
-    // text(" :", width / 2 + 25, height / 2);
-    // text(min, width / 2 + 50, height / 2);
-    // text(" :", width / 2  + 75, height / 2);
-    // text(sec, width / 2 + 100, height / 2);
-}
+//     // text(hour, width / 2, height / 2);
+//     // text(" :", width / 2 + 25, height / 2);
+//     // text(min, width / 2 + 50, height / 2);
+//     // text(" :", width / 2  + 75, height / 2);
+//     // text(sec, width / 2 + 100, height / 2);
+// }
+
